419e8cb67ac8f02a554b5a8326ed5c052bbd3c3d
AnnaUgrai <ugraianna@gmail.com>
2024-06-13 20:27:43 +0200

    a3

--- group_2/src/Count.java
+++ group_2/src/Count.java
@@ -1,11 +1,11 @@
 public class Count {
     public static void main(String[] args) {
 
-        count(40);
+        count(11);
 
     }
 
     private static void count(int number) {
-        System.out.println("Age: " + number);
+        System.out.println("The month I was born in: " + number);
     }
 }
