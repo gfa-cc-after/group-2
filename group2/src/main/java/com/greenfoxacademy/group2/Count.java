9244c2ada1feda82918c553aef426d75a7a19793
HsBalázs <hoffartscharfi@t-online.hu>
2024-06-13 19:05:12 +0200

    b1

--- group2/src/main/java/com/greenfoxacademy/group2/Count.java
+++ group2/src/main/java/com/greenfoxacademy/group2/Count.java
@@ -2,6 +2,6 @@ package main.java.com.greenfoxacademy.group2;
 
 public class Count{
     public static void main(String[] args) {
-        System.out.println("Age:" + 40);
+        System.out.println("Age:" +  40);
     }
 }
