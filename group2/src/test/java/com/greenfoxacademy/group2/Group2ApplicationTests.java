7ffc22576c653dd8e72a37063532a1f9acc88327
Hsbalazs <hoffartscharfi@t-online.hu>
2024-06-13 19:11:28 +0200

    b1

---
+++ group2/src/test/java/com/greenfoxacademy/group2/Group2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
