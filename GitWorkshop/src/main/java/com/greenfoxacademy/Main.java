30b36248e4cf5bda2c36eceefed1869f2c70d91b
Carlos Ponton <carlosponton20@gmail.com>
2024-06-13 19:05:41 +0200

    adding greet method

---
+++ GitWorkshop/src/main/java/com/greenfoxacademy/Main.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        greet();
+
+    }
+
+    public static void greet() {
+        System.out.println("Carlos");
+    }
+}
