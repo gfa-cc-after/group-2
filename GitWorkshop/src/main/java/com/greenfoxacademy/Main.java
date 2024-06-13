cb3a84d7e4aba04ec089da0ac4f72acc11ee1dba
Ramon Trekovanicz <ramesz6@me.com>
2024-06-13 19:54:08 +0200

    d2

--- GitWorkshop/src/main/java/com/greenfoxacademy/Main.java
+++ GitWorkshop/src/main/java/com/greenfoxacademy/Main.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy;
+package main.java.com.greenfoxacademy;
 
 public class Main {
     public static void main(String[] args) {
--- future/src/Future.java
+++ future/src/Future.java
@@ -1,5 +1,7 @@
 public class Future {
+    public static String sign;
     public static void main(String[] args) {
-        System.out.println("Leo");
+        sign = "Leo";
+        System.out.println(sign);
     }
 }
